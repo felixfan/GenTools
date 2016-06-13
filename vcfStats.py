2738c6f9eb2ec8a84e3d220432874f57dcc6553c
Felix <felixfanyh@gmail.com>
2016-06-13 22:17:36 +0800

    Individual Heterozygosity

--- vcfStats.py
+++ vcfStats.py
@@ -6,6 +6,9 @@ import sys
 import operator
 
 def stat_info(infile, outfile, key):
+    '''
+    calculate how many of each value for the key in the INFO field.
+    '''
     fr = open(infile)
     fw = open(outfile, 'w')
     n = 0
@@ -38,6 +41,9 @@ def stat_info(infile, outfile, key):
     fr.close()
 
 def combine_vcf(infile1, infile2, outfile):
+    '''
+    concatenates VCF files, input file should have same columns.
+    '''
     n = 0
     m = 0
     z = 0
@@ -89,6 +95,76 @@ def run_time(starttime):
     print "Finished at ",
     print time.strftime("%H:%M:%S %d %b %Y")
 
+def cal_tstv(infile, outfile):
+    '''
+    calculates transition transversion ratio, only use biallelic variants
+    '''
+    pass
+
+def cal_het(infile, outfile):
+    '''
+    calculates heterozygosity on a per-individual basis, only use biallelic variants
+    '''
+    fr = open(infile)
+    fw = open(outfile, 'w')
+    het = []
+    hom = []
+    ind = []
+    n = 0
+    for r in fr:
+        r = r.strip()
+        if r.startswith("##"):
+            pass
+        elif r.startswith("#"):
+            ind = r.split()[9:]
+            n = len(ind)
+            het = [0] * n
+            hom = [0] * n
+        else:
+            arr = r.split()
+            if arr[4].find(',') == -1:
+                for i in xrange(n):
+                    gtp = arr[i+9][0:3]
+                    if gtp.count('0') == 1:
+                        het[i] += 1
+                    else:
+                        hom[i] += 1
+    fr.close()
+    print 'Outputting Individual Heterozygosity: Only using biallelic variants.\n'
+    fw.write('{}\t{}\t{}\t{}\n'.format('indvID', 'numHet', 'numHom', 'HetRatio'))
+    print 'indvID', 'numHet', 'numHom', 'HetRatio'
+    for i in xrange(n):
+        hetratio = 1.0 * het[i] / (het[i] + hom[i])
+        fw.write('{}\t{}\t{}\t{}\n'.format(ind[i], het[i], hom[i], hetratio))
+        print ind[i], het[i], hom[i], hetratio
+    fw.close()   
+    print "\nWrite results to {}".format(outfile)
+
+def convert_plink_tped(infile, outfile):
+    '''
+    output the genotype data in PLINK tped/tfam format, only biallelic variants will be output.
+    '''
+    pass
+
+def diff_site(infile1, infile2, outfile):
+    '''
+    output sites that are common /unique to each file
+    '''
+    pass
+
+def diff_indv(infile1, infile2, outfile):
+    '''
+    output individuals that are common /unique to each file
+    '''
+    pass
+
+def basic_stats(infile, outfile):
+    '''
+    output some basic statistics: number of individuals, total number of variants, number of SNPs and indels,
+    number of biallelic variants, multiple allelic variants
+    '''
+    pass
+
 if __name__ == '__main__':
     starttime = time.time()
     desc = '''Python tool for calculating some basic statistics of VCF file'''
@@ -101,6 +177,7 @@ if __name__ == '__main__':
     parser.add_argument('--key', help='key words to be used', type=str)
     parser.add_argument('--comb-vcf', help='combine two vcf files', action='store_true')
     parser.add_argument('--vcf2', help='the second input vcf file', type=str)
+    parser.add_argument('--het', help='calculates heterozygosity on a per-individual basis', action='store_true')
     ### output
     parser.add_argument('--out', help='output file', type=str, default='output.txt')
     ### parameter
@@ -111,9 +188,10 @@ if __name__ == '__main__':
     KEY = args['key'] if 'key' in args else None
     COMBVCF = args['comb_vcf'] if 'comb_vcf' in args else False
     INFILE2 = args['vcf2'] if 'vcf2' in args else None
+    HET = args['het'] if 'het' in args else False
     ### log
     print "@-------------------------------------------------------------@"
-    print "|        vcfStats       |      v0.1.0       |   13 Jun 2016   |"
+    print "|        vcfStats       |      v0.2.0       |   13 Jun 2016   |"
     print "|-------------------------------------------------------------|"
     print "|  (C) 2016 Felix Yanhui Fan, GNU General Public License, v2  |"
     print "|-------------------------------------------------------------|"
@@ -134,6 +212,8 @@ if __name__ == '__main__':
             print '\t--vcf2', INFILE2
         else:
             sys.exit('option --vcf2 is missing')
+    elif HET:
+        print '\t--het'
     print "\t--out", OUTFILE
     print
     ### run
@@ -141,6 +221,8 @@ if __name__ == '__main__':
         stat_info(INFILE, OUTFILE, KEY)
     elif COMBVCF:
         combine_vcf(INFILE, INFILE2, OUTFILE)
+    elif HET:
+        cal_het(INFILE, OUTFILE)
     else:
         print "Do nothing"
     ###time
