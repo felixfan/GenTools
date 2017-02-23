902558feb861150812c9010feec1f433693d2733
Yanhui Fan <felixfanyh@gmail.com>
2017-02-23 15:12:02 +0800

    Update pyVcfFilter.py

--- pyVcfFilter.py
+++ pyVcfFilter.py
@@ -1027,7 +1027,7 @@ if __name__ == '__main__':
     print "|  (C) 2016 Felix Yanhui Fan, GNU General Public License, v2  |"
     print "|-------------------------------------------------------------|"
     print "|    For documentation, citation & bug-report instructions:   |"
-    print "|            http://felixfan.github.io/PyTV                   |"
+    print "|            http://felixfan.github.io/GenTools               |"
     print "@-------------------------------------------------------------@"
     print "\n\tOptions in effect:"
     print "\t--vcf", INFILE
